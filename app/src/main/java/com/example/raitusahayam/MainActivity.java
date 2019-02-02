fe47442cc838679d55356c9a4003f67834c6a8e1
Yogesh Ojha <yogesh.ojha11@gmail.com>
2019-02-02 12:58:50 +0530

    nav bar done

--- app/src/main/java/com/example/raitusahayam/MainActivity.java
+++ app/src/main/java/com/example/raitusahayam/MainActivity.java
@@ -4,10 +4,18 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fm = getSupportFragmentManager();
+    final Fragment dailyFragment = new DailyCommodity();
+    final Fragment chatbot = new ChatBotFragment();
+    final Fragment settings = new Settings();
+    final Fragment realtime = new RealtimeFragment();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -15,7 +23,18 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-
+                case R.id.navigation_daily:
+                    fm.beginTransaction().replace(R.id.container_frame, dailyFragment, "home").commit();
+                    return true;
+                case R.id.navigation_chatbot:
+                    fm.beginTransaction().replace(R.id.container_frame, chatbot, "home").commit();
+                    return true;
+                case R.id.navigation_settings:
+                    fm.beginTransaction().replace(R.id.container_frame, settings, "home").commit();
+                    return true;
+                case R.id.navigation_realtime:
+                    fm.beginTransaction().replace(R.id.container_frame, realtime, "home").commit();
+                    return true;
             }
             return false;
         }
@@ -27,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        fm.beginTransaction().replace(R.id.container_frame, dailyFragment, "Home").commit();
     }
 
 }
